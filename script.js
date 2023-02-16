f3e4ab2d3af93bad6f3b9334d00168b0470e3982
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-02-16 03:02:21 -0500

    Added function which completes operation of values
    This function occurs when clicking equals to complete an operation after
    the operator has already been chosen. This function also displays results
    and afterwards clears stored values to store result. This is so it the
    result is ready to be used in next operation if desired by user.

--- script.js
+++ script.js
@@ -15,36 +15,52 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
-    if (operator === '+') {
+    if (operator === 'add') {
         return add(num1, num2);
-    } else if (operator === '-') {
+    } else if (operator === 'subtract') {
         return subtract(num1, num2);
-    } else if (operator === '*') {
+    } else if (operator === 'multiply') {
         return multiply(num1, num2);
-    } else if (operator === '/') {
+    } else if (operator === 'divide') {
         return divide(num1, num2);
     } 
 }
 
 function populateDisplay(e) {
+    if (displayCounter) {
+        display.textContent = '';
+        displayCounter = false;
+    }
     display.textContent += this.textContent;
 }
 
-function operation(e) {
+function startOperation(e) {
     if (this.textContent === 'AC') {
         display.textContent = '';
+        operand = '';
         storedValues = [];
         return
     }
 
     operand = this.id;
-    storedValues.push(display.textContent);
-    display.textContent = '';  
-    console.log(storedValues); 
+    displayCounter = true;
+    /* if (storedValues.length === 2) {} */
+    storedValues.push(Number(display.textContent));  
+}
+
+function finishOperation() {
+    storedValues.push(Number(display.textContent));
+    display.textContent = '';
+
+    display.textContent = operate(operand, storedValues[0], storedValues[1]);
+    storedValues = [];
+    storedValues.push(Number(display.textContent));
+    console.log(storedValues);
 }
 
 let storedValues = [];
-let operand;
+let operand = '';
+let displayCounter = false;
 
 const display = document.querySelector('.display');
 
@@ -52,4 +68,7 @@ const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => number.addEventListener('click', populateDisplay));
 
 const operators = document.querySelectorAll('.operator');
-operators.forEach(operator => operator.addEventListener('click', operation));
+operators.forEach(operator => operator.addEventListener('click', startOperation));
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', finishOperation);
